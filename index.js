0e07825576fc49c2793062765153413a76264de0
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-06 11:20:25 +0600

    Check whether version is newer. Add debugging

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 import core from '@actions/core';
 import {execa} from 'execa';
 import fsExtra from 'fs-extra';
+import debug from 'debug';
+import semver from 'semver'
 
 async function run() {
+  const log = debug(`is-npm-package-version-changed-github-action`)
   try {
     const packageJson = fsExtra.readJsonSync('./package.json');
+    log({packageJson})
     const packageName = packageJson.name;
+    log({packageName})
     const currentPackageVersion = packageJson.version;
+    log({currentPackageVersion})
 
     const { stdout: npmPackageVersion } = await execa(`npm`, [`view`,packageName, `version`]);
+    log({npmPackageVersion})
 
-    const isVersionChanged = currentPackageVersion !== npmPackageVersion.trim();
+    const isVersionChanged = semver.gt(currentPackageVersion, npmPackageVersion.trim());
+    log({isVersionChanged})
+
     core.setOutput('is-version-changed', isVersionChanged.toString());
   } catch (error) {
     core.setFailed(error.message);
